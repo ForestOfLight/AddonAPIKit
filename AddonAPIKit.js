cc8a7507d9fc183575566f1c5a55187bbf5925a2
ForestOfLight <briancassriel@gmail.com>
2026-05-30 00:33:38 +0200

    feat: add untested API handling

---
+++ AddonAPIKit.js
@@ -0,0 +1,8 @@
+import { AddonAPI } from "./AddonAPI";
+import { APIController } from "./APIController";
+import { VoidModel } from "./APIModels";
+import { APICallerError } from "./Errors/APICallerError";
+import { PROTO } from "./MCBE-IPC/ipc";
+import { AddonAPICaller } from "./AddonAPICaller";
+
+export { AddonAPI, APIController, VoidModel, APICallerError, PROTO, AddonAPICaller };
